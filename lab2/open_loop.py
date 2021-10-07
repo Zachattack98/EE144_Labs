2512d1aed60c0a880eeb76dafa99549eefecbb6e
Zachattack98 <bakugan0498@yahoo.com>
2021-10-07 11:37:10 -0700

    Adjusting rotations at each corner of square

--- lab2/open_loop.py
+++ lab2/open_loop.py
@@ -17,28 +17,40 @@ class Turtlebot():
 
     def run(self):
         vel = Twist()
-	      cnt = 0
-	      cnt2 = 0
+	cnt = 0
+	cnt2 = 0
+	# start by readjusting the initial position to keep the bot on the x-axis
+	vel.linear.x = 0
+        vel.angular.z = pi/16
+	for i in range(4):
+        	self.vel_pub.publish(vel)
+        	self.rate.sleep()
         while not rospy.is_shutdown():  # uncomment to use while loop
-		      if cnt < 4:
-			      vel.linear.x = 0.5
+		if cnt < 4:
+			vel.linear.x = 0.5
         		vel.angular.z = 0
         		for i in range(80):
             			self.vel_pub.publish(vel)
             			self.rate.sleep()
 
-			      vel.linear.x = 0	    #robot remains in stationary position
-            vel.angular.z = pi/4	#rotate counter-clockwise (right-hand rule)
-						                      #decreasing value slows the rotation speed
-			      while cnt2 < 20:
+			vel.linear.x = 0	#robot remains in stationary position
+            		vel.angular.z = pi/4	#rotate counter-clockwise (right-hand rule)
+						#decreasing value slows the rotation speed
+			while cnt2 < 20:   #start rotating for a set numbers of 0.1 secs	
             			self.vel_pub.publish(vel)
             			self.rate.sleep()
-	    			      cnt2 += 1
+	    			cnt2 += 1
 
-			      cnt += 1
-			      cnt2 = 0
-		      else:
-			      break
+			#another rotation adjustment; still stationary but smaller
+        		vel.angular.z = pi/16
+			for i in range(7):
+        			self.vel_pub.publish(vel)
+        			self.rate.sleep()
+
+			cnt += 1
+			cnt2 = 0
+		else:
+			break
 
 
 if __name__ == '__main__':
