88e3e03b71fa2108f20a91a6b41b03becc6ebae6
Zachattack98 <bakugan0498@yahoo.com>
2021-10-27 22:09:33 -0700

    Finalized functions for unknown variables!

--- lab4/inverse_kinematics.py
+++ lab4/inverse_kinematics.py
@@ -11,27 +11,30 @@ def inverse_kinematics(position):
     link3x = 0.050
     link3z = 0.150
     link4x = 0.150
-    x = position[0]
-    y = position[1]
-    z = position[2]
+    x = position[0]	#x direction points east
+    y = position[1]     #y direction points north
+    z = position[2]     #z direction points straight up, like a pole in the middle
 
     #Finding joint 1 using theta 1 only
     theta1 = atan2(y,x)
     joint1 = theta1
 
     #Finding joint 2 using theta 2 and other terms
-    sqX = sqrt(x**2 + y**2)
+    #y_1 = (x/cos(theta1))
+    coord_XY = sqrt(x**2 + y**2)       #result of the hypotenuse for square rooting the coordinates x and y
+    hypG = sqrt(coord_XY**2 + (z - 0.065 - 0.0389)**2)    #hypotenuse of gamma, or red and blue triangles; 65 + 38.9 is the offset
     alpha = atan2(0.05, 0.15)	#convert the given values from mm to cm
-    beta1 = acos(((0.1501**2) + (sqX**2) + (z**2) - (0.15**2)) / (2 * 0.1581 * sqrt(sqX**2 + z**2)))
+    beta1 = acos(((0.1581**2) + (hypG**2) - (0.15**2)) / (2 * 0.1581 * hypG))
     gamma = atan2(z,x)
-
+    
     theta2 = (pi/2) - alpha  - beta1 - gamma
     joint2 = theta2
 
     #Finding joint 3 using theta 3
-    beta2 = acos(((0.1501**2) + (0.15**2) - (sqX**2) + (z**2)) / (2 * 0.1581 + 0.15))
-    betaTemp = pi - beta2
-    theta3 = pi - alpha - pi/2 - betaTemp
+    beta2 = acos(((0.1581**2) + (0.15**2) - (hypG**2)) / (2 * 0.1581 * 0.15))
+    betaTemp = pi - beta2        #imagine cutting the yellow hypotenuse angle into two smaller angles, resulting in two new triangles. Gives us larger yellow triangle's hypotenuse angle.
+    
+    theta3 = pi - alpha - (pi/2) - betaTemp   #(all angles of yellow triangle) - (alpha) - (right angle form at top of yellow triangle) - (betaTemp) = smallest angle of snaller yellow triangle which is mirror of theta3.
     joint3 = theta3
 
     return [joint1, joint2, joint3]
