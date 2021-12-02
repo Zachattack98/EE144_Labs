4077a25a6dfc51984dc904477d14b0a41fc75a39
Zachattack98 <bakugan0498@yahoo.com>
2021-12-01 21:47:06 -0800

    Finalized file!

--- lab6/trajectory_generation.py
+++ lab6/trajectory_generation.py
@@ -1,3 +1,5 @@
+
+
 #!/usr/bin/env python
 
 from math import pi, sqrt, atan2, cos, sin
@@ -28,6 +30,10 @@ class Turtlebot():
         self.logging_counter = 0
         self.trajectory = list()
         self.odom_sub = rospy.Subscriber("odom", Odometry, self.odom_callback)
+        self.previous_waypoint = [0,0]
+        self.previous_velocity = [0,0]
+        self.vel_ref = 0.3
+        self.vel = Twist()
 
         try:
             self.run()
@@ -39,130 +45,80 @@ class Turtlebot():
 
 
     def run(self):
-        waypoints = [[0.5, 0], [0.5, -0.5], [1, -0.5], [1, 0], [1, 0.5],\
-                      [1.5, 0.5], [1.5, 0], [1.5, -0.5], [1, -0.5], [1, 0],\
-                      [1, 0.5], [0.5, 0.5], [0.5, 0], [0, 0], [0, 0]]
+        waypoints = [[0.5, 0], [0.5, -0.5], [1, -0.5], [1, 0], [1, 0.5], [1.5, 0.5], [1.5, 0], [1.5, -0.5], [1, -0.5], [1, 0], [1, 0.5], [0.5, 0.5], [0.5, 0], [0, 0], [0, 0]]
         for i in range(len(waypoints)-1):
             self.move_to_point(waypoints[i], waypoints[i+1])
 
     def move_to_point(self, current_waypoint, next_waypoint):
-        self.previous_waypoint = [0,0]
-        self.previous_velocity = [0,0]
-	self.vel_ref = 0.3
-	self.vel = Twist()
-        
         # generate polynomial trajectory and move to current_waypoint      
         # next_waypoint is to help determine the velocity to pass current_waypoint
         #determine boundary conditions for the position for x and y
         px_start = self.previous_waypoint[0]
         px_end = current_waypoint[0]
-
+        #print(px_end)
         py_start = self.previous_waypoint[1]
         py_end = current_waypoint[1]
 
         #determine boundary  conditions for velocity
         #need to compute the angle of the robot in order to change the x,y velocities
-        vx_start = self.previous_velocity[0]   #velocity*cos()
-        vy_start = self.previous_velocity[1]   #velocity*sin()
+        vx_start = self.previous_velocity[0]    #velocity*cos()
+        vy_start = self.previous_velocity[1]    #velocity*sin()
 
         #decompose the velocity
         dx = next_waypoint[0] - current_waypoint[0]
         dy = next_waypoint[1] - current_waypoint[1]
         angle = atan2(dy,dx)
 
-        vx_end = self.vel_ref*cos(angle)
-        vy_end = self.vel_ref*sin(angle)
+        if(current_waypoint[0] == 0 and current_waypoint[1] == 0 and next_waypoint[0] == 0 and next_waypoint[1] == 0):  #here the robot should stop moving forward since vx and vy take part in determining linear velocity
+            vx_end = 0  
+            vy_end = 0
+        else:       
+            vx_end = self.vel_ref*cos(angle)  
+            vy_end = self.vel_ref*sin(angle)
 
+        print(vx_end)
+        print(vy_end)
         #solve the polynomial coefficient
         #going to get coefficients for x and coefficients for y
-        T = 2  #any value
-
+        T = 2
+        Kp = 5
         coeff_x = self.polynomial_time_scaling_3rd_order(px_start, vx_start, px_end, vx_end, T)
         coeff_y = self.polynomial_time_scaling_3rd_order(py_start, vy_start, py_end, vy_end, T)
-
+        #print(coeff_x)
+        #print(coeff_y)
         #now we want the robot to move, we have the coefficients now which can be used to find the velocity.
         #The velocity can now be sent to twist for a specified range of time values hint: use for loop
         
-        for i in range(0, 5*T)
+        for i in range(0, 10*T):
             #send the velocity
             #we can play with t and extend the time to see what happens
-            t = i * 1
+            t = i * 0.1 
             #compute Vx_end, Vy_end; get vx vy, then we go from velocity to angle
-            Vx_end = [3*t^2, 2*t, 1, 0]*[coeffx]  # ([row] * [col]); t = i * 0,1 (choose 0 or 1)
-            Vy_end = [3*t^2, 2*t, 1, 0]*[coeffy]  # same t
+            vx_end = np.dot([3*t**2, 2*t, 1, 0], coeff_x) # ([row] * [col]); t = i * 0,1 (choose 0 or 1)
+            vy_end = np.dot([3*t**2, 2*t, 1, 0], coeff_y)  # np.dot is done since its a matrix multiplication
+            theta = atan2(vy_end, vx_end)    #re-calculate angle theta
+            dtheta = theta - self.pose.theta # delta theta = starting theta - current theta 
+            #print(theta)
+
+            #here we change theta to be within the range of Gazebo (-pi, pi)
+            if (dtheta < -pi): 
+                self.vel.angular.z = Kp*(dtheta + 2*pi)
+            elif (dtheta > pi):
+                self.vel.angular.z = Kp*(dtheta - 2*pi)
+                
+            else:
+                self.vel.angular.z = Kp*dtheta
             
-            #Code from lab 3 closed loop
-            cnt = 0	                            
-	    theta_error = 0	                    #Variable for e(t); compute theta
-	    #while not rospy.is_shutdown():
-	    turn_theta = cnt*(pi/2)		#Variable for turning at each point; 2D means we can stick with pi/2 to rotate 90 degrees.
-	    delta_theta_error = theta_error	    #Temporary variable to store the lastest theta error; compute delta theta (self.pose.theta)
-	    K_p = 3		                
-	    K_d = 0.5
+            self.vel.linear.x = sqrt(vx_end**2 + vy_end**2)
 
-            if cnt < 2:                       #Condition check to reduce redundant computations
-                theta_error = (turn_theta - self.pose.theta)
-                theta_derivative = (theta_error - delta_theta_error) #Derivative of theta_error
-		    if(abs(theta_error) < 0.05):	
-			    self.vel.linear.x = 0.5		
-		    else:					        
-			    self.vel.linear.x = 0
-		    if (cnt == 0 and abs(self.pose.x - 2) < 0.07):	#Using odom readings we can tell when we reach a corner; side 1
-			    cnt += 1					                    
-			    self.vel.linear.x = 0		        #Reset linear velocity before turning.
-		    elif (cnt == 1 and abs(self.pose.y - 2) < 0.07):    #side 2
-			    cnt += 1
-			    self.vel.linear.x = 0
-		    elif (cnt == 2):  #side 3
-			    #Since Gazebo's range is (-pi, pi) we cannot use 3*pi/2, or else the bot spins out of control. Redo calculations     
-		        if (self.pose.theta < 0):
-				    theta_error = (-(pi) - self.pose.theta)
-			    else:
-				    theta_error = (turn_theta - self.pose.theta)
-           		    theta_derivative = (theta_error - delta_theta_error)
-			    if (abs(self.pose.x - 0) < 0.07):
-				    cnt += 1		
-				    self.vel.linear.x = 0
-		    elif (cnt == 3):    #side 4
-			    if (self.pose.theta < 0):
-				    theta_error = (-(pi/2) - self.pose.theta)
-			    else:
-				    theta_error = (turn_theta - self.pose.theta)
-           		    theta_derivative = (theta_error - delta_theta_error)
-			    if ((abs(self.pose.y - 0)) < 0.07):
-				    cnt += 1
-				    self.vel.linear.x = 0			  
-		
-		    self.vel.angular.z = (K_p * theta_error) + (K_d * theta_derivative)	#Angular velocity using given u(t) equation; 
-											                                    #k_p*e(t) + k_d*(e(t) - e(t - delta(t)))
-                                                                                #update velocity = Kp * delta theta
-		
-		    self.vel_pub.publish(self.vel)  #Publish velocities
-		    if (cnt == 4):
-			    self.vel.angular.z = pi/2 + pi/4 + pi/8		#Reset to initial direction; pi was too much
-			    self.vel_pub.publish(self.vel)
-			    break
-		    self.rate.sleep()
+            self.vel_pub.publish(self.vel)
+            	                            
+            self.rate.sleep()
             
-            #Update the previous values
-            px_start = current_waypoint[i+1]
-            px_end = next_waypoint[i+2]
-
-            py_start = current_waypoint[i+2]
-            py_end = next_waypoint[i+3]
-
-            vx_start = current_velocity[i+1]  #velocity*cos()
-            vy_start = current_velocity[i+2]  #velocity*sin()
-            
-            dx = next_waypoint[0] - current_waypoint[0]
-            dy = next_waypoint[1] - current_waypoint[1]
-            angle = atan2(dy,dx)
-
-            vx_end = self.vel_ref*cos(angle)
-            vy_end = self.vel_ref*sin(angle)
-
-            coeff_x = self.polynomial_time_scaling_3rd_order(px_start, vx_start, px_end, vx_end, T)
-            coeff_y = self.polynomial_time_scaling_3rd_order(py_start, vy_start, py_end, vy_end, T)
+        #Update the previous values
+        self.previous_waypoint = current_waypoint
+        self.previous_velocity = [vx_end, vy_end]
+        pass
  
         # implement a PID controller similar to lab 3 
         # 
@@ -173,8 +129,11 @@ class Turtlebot():
         # T: the desired time to complete this segment of trajectory (in second)
         # output: the coefficients of this polynomial
         A = np.array([[p_start], [p_end], [v_start], [v_end]])
-        B = np.array([[0, 0, 0, 1],[T*T*T, T*T, T, 1],[0, 0, 1, 0],[3*T*T, 2*T, 1, 0]])
-        return (np.linalg.inv(A))*B
+        B = np.array([[0, 0, 0, 1],[T**3, T**2, T, 1],[0, 0, 1, 0],[3*T**2, 2*T, 1, 0]])
+        binv = np.linalg.inv(B)
+        return((np.dot(binv, A)))
+        
+        #print((np.linalg.inv(B)*A))
 
 
     def odom_callback(self, msg):
